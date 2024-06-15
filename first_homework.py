01f92ee9cee355534b357c062a5e70fcdf0ab27f
Vasyl <bilonozhko.va@gmail.com>
2024-06-15 17:15:58 +0300

    Added file my first homework

--- first_homework.py
+++ first_homework.py
@@ -1,1 +1,4 @@
-my_favorite_city = 'З'
+my_favorite_city = 'Kyiv'
+my_association_with_city_in_emoji = '🏠⛪🏢🏣🚄🚅'
+
+print('My favorite city is:', my_favorite_city, 'and my association in emoji with it is:', my_association_with_city_in_emoji)
