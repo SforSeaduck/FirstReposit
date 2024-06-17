5f3024e1ec5568a8193bf4a37daaf4dcee414e2c
Vasyl <bilonozhko.va@gmail.com>
2024-06-17 13:26:55 +0300

    added homework 7

--- funcshions.py
+++ funcshions.py
@@ -1,6 +1,12 @@
+from pywebio.output import *
+from pywebio.input import input as input_pw
 
-def generate_html_heading(username):
+def html_headers_creator(username):
 
-    html_heading = f"<h1>Вітаємо тебе, шановний {username}</h1>"
+    username = input_pw('Як вас звати?')
+    html_header = f"<h1>Вітаємо тебе шановний, {username}</h1>"
+    put_html(f"<h1>{html_header}</h1>")
 
-    return html_heading
+    return html_header
+
+html_headers_creator('username')
